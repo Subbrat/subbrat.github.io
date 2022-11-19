3cd694c858b656d179e6d9a92b873ac6fad704ea
Subrat Mallick <m.subrat198470@gmail.com>
2022-11-19 13:04:09 +0530

    ASDAD

--- test.js
+++ test.js
@@ -1,6 +1,27 @@
-onclick = "printFunction()"
-
-    < script >
-    function printFunction() {
-        window.print();
+{
+    function formcheck(that) {
+        if (that.value == "webdev") {
+            document.getElementById("webdeform").style.display = "block";
+            document.getElementById("formtype").style.display = "none";
+        }
+        else if (that.value == "sound") {
+            document.getElementById("soundeng").style.display = "block";
+            document.getElementById("formtype").style.display = "none";
+        }
+        else {
+            document.getElementById("webdeform", "soundeng").style.display = "none";
+        }
     }
+    const checkbox = document.querySelector('.frm input[type="checkbox"]');
+    const btns = document.querySelectorAll(".frm button");
+    checkbox.addEventListener("change", function () {
+        const checked = this.checked;
+        for (const btn of btns) {
+            checked ? (btn.disabled = false) : (btn.disabled = true);
+        }
+    });
+}
+{
+
+
+}
